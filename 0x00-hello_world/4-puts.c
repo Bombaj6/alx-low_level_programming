56f8fbba7b1c735188966714f008a037b12e30b6
bombaj6 <bombaj6@gmail.com>
2022-09-08 07:32:37 -0700

    hello

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Sucess)
+ * /
+ int main(void)
+ {
+ 	char Mystring[]="Programming is like building a multilingual puzzle";
+	puts(Mystring);
+	return(0);
+}
