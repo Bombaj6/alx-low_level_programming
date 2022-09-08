5f9a79b70d46d4a1e781ce004b0347fd89ce474a
bombaj6 <bombaj6@gmail.com>
2022-09-08 12:42:46 -0700

    alx

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 
 /**
- *  main - Entry point
+ * main - Entry point
  *
- *  Return: Always 0 (Success)
+ * Return: Always 0 (Sucess)
+ *
  */
+
 int main(void)
-
 {
-	char Mystring[] = '"Programming is like building a multilingual puzzle';
+	char Peter[] = "\"Programming is like building a multilingual puzzle";
 
-	puts(Mystring);
+	puts(Peter);
 
 	return (0);
 }
