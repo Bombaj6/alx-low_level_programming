a1e0ca4b136b690718aefa0882bfd95edaa26b79
bombaj6 <bombaj6@gmail.com>
2022-09-09 07:17:22 -0700

    alx

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Numbers prints five
+ *
+ * Return:Always 0 (Sucess)
+ *
+ */
+int main(void)
+{
+	char n;
+
+	for (n = '0' ; n <= '9'; n++)
+	{
+		putchar(n);
+	}
+
+	putchar('\n');
+
+		return (0);
+}
